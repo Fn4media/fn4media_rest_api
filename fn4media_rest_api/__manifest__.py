108d029b5483d69f3a9d53cb2503c73e546779a4
Fn4media <37480690+Fn4media@users.noreply.github.com>
2018-05-02 00:55:37 +0530

    Add files via upload

---
+++ fn4media_rest_api/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fn4media REST API',
+    'summary': '''
+        RES API (json) access to Odoo models
+    ''',
+    'description': '''
+        REST API (json) access to Odoo models
+    ''',
+    'license':'OPL-1',
+    'author': 'Fn4media Technologies Pvt Ltd ',
+    'website': 'http://www.fn4media.in/apps',
+    'category': 'Extra Tools',
+    'version': '0.1',
+    'images': ['static/description/banner.png'],
+    'depends': ['auth_oauth'],
+    'currency' : 'EUR',
+    'price' : 60,
+}
